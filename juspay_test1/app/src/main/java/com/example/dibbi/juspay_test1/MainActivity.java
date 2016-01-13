e2a1ca5fabaaac47490032c3f22833bb60dde34e
divya2661 <divn2661@gmail.com>
2016-01-14 04:49:19 +0530

    JuspayTest1 complete

--- juspay_test1/app/src/main/java/com/example/dibbi/juspay_test1/MainActivity.java
+++ juspay_test1/app/src/main/java/com/example/dibbi/juspay_test1/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -16,26 +17,33 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.example.dibbi.juspay_test1.AppController;
 
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-     public static String url = "http://divya2661.netau.net/test.json";
+    public static String url = "http://divya2661.netau.net/test.json";
     createComponent comp = new createComponent(this);
     private ProgressDialog pDialog;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        relativeLayout = (RelativeLayout)findViewById(R.id.content_Main);
+        Log.d("okay", "Coming to update11");
         //progress dialog
         pDialog = new ProgressDialog(this);
         pDialog.setCancelable(false);
+        Log.d("okay", "Coming to update22");
         getJson();
+
+        Log.d("okay", "Coming to update555");
 
     }
 
@@ -43,16 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
         pDialog.setMessage("getting json data");
         showDialog();
+        Log.d("okay", "Coming to update666");
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
             @Override
             public void onResponse(String response) {
 
                 try {
-
+                    Log.d("okay","Coming to update333");
                     JSONObject jObj = new JSONObject(response);
 
                     updateUI(jObj);
+                    hideDialog();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -72,7 +83,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUI(JSONObject jObj) {
-        
+
+        try {
+
+            Log.d("okay","Coming to update44");
+            JSONArray components = jObj.getJSONArray("Rlayout");
+            JSONObject obj1 = components.getJSONObject(0);
+            Log.d("okay","Coming: "+obj1.getString("Object"));
+
+            if(obj1.getString("Object").equals("textView")){
+
+                Log.d("okay","Coming in");
+                int layout_width = obj1.getInt("layout_width");
+                int layout_height = obj1.getInt("layout_height");
+                String layout_text = obj1.getString("text");
+                int textSize = obj1.getInt("textSize");
+                String id = obj1.getString("id");
+
+                Log.d("okay","Coming text: " + layout_text + "  "+layout_width + " " + layout_height + "  "+textSize);
+
+                TextView textView = new TextView(this);
+
+                textView.setWidth(layout_width);
+                textView.setHeight(layout_height);
+                textView.setText(layout_text);
+                textView.setTextSize(textSize);
+                textView.setTag(id);
+
+                relativeLayout.addView(textView);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- juspay_test1/app/src/main/java/com/example/dibbi/juspay_test1/createComponent.java
+++ juspay_test1/app/src/main/java/com/example/dibbi/juspay_test1/createComponent.java
@@ -1,7 +1,10 @@
 package com.example.dibbi.juspay_test1;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by dibbi on 14/1/16.
@@ -19,4 +22,17 @@ public class createComponent {
         Button button = new Button(context);
         return button;
     }
+
+    public TextView createTextView(){
+
+        TextView textView = new TextView(context);
+        return textView;
+    }
+
+    public EditText createEditText(){
+
+        EditText editText = new EditText(context);
+        return editText;
+    }
+
 }
